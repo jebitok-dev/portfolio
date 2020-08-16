3bd85d1d42b2a35e7c5ffdd605b46a5f802fef8c
jebitok-dev <sharonkosgei4@gmail.com>
2020-08-17 02:42:57 +0300

    Updates

---
+++ precache-manifest.767e19012dc021ba8fc8f223d12c5cbc.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6220f699d799760c46f11e5891a3569",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7321987424d61abcc105",
+    "url": "/static/css/3.c7b31c95.chunk.css"
+  },
+  {
+    "revision": "82463860089881c27e03",
+    "url": "/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "ebaaad0e71873c4986d8",
+    "url": "/static/css/main.5bd13985.chunk.css"
+  },
+  {
+    "revision": "d2c4e48a25dc80c61e62",
+    "url": "/static/js/2.e189d8f3.chunk.js"
+  },
+  {
+    "revision": "7321987424d61abcc105",
+    "url": "/static/js/3.2ab96c8f.chunk.js"
+  },
+  {
+    "revision": "82463860089881c27e03",
+    "url": "/static/js/4.08e0158b.chunk.js"
+  },
+  {
+    "revision": "ebaaad0e71873c4986d8",
+    "url": "/static/js/main.0b722e51.chunk.js"
+  },
+  {
+    "revision": "25ed2f5b993a30230346",
+    "url": "/static/js/runtime-main.8a6a267d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
