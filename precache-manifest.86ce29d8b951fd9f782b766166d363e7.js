046c82abf676a553caa01df48b75d671d51db80b
jebitok-dev <sharonkosgei4@gmail.com>
2020-08-17 02:47:50 +0300

    Updates

---
+++ precache-manifest.86ce29d8b951fd9f782b766166d363e7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8a8257514db754c9ed5a27413e08ed",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d60ba019969b10462200",
+    "url": "/portfolio/static/css/3.c7b31c95.chunk.css"
+  },
+  {
+    "revision": "33dc16fb2452584f652f",
+    "url": "/portfolio/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "fc07c6686e2c6d25e52c",
+    "url": "/portfolio/static/css/main.666c3db5.chunk.css"
+  },
+  {
+    "revision": "5f3a504cc22bdec27f1d",
+    "url": "/portfolio/static/js/2.fd515ca6.chunk.js"
+  },
+  {
+    "revision": "d60ba019969b10462200",
+    "url": "/portfolio/static/js/3.e52b9c71.chunk.js"
+  },
+  {
+    "revision": "33dc16fb2452584f652f",
+    "url": "/portfolio/static/js/4.50027914.chunk.js"
+  },
+  {
+    "revision": "fc07c6686e2c6d25e52c",
+    "url": "/portfolio/static/js/main.00f825fd.chunk.js"
+  },
+  {
+    "revision": "f87d49553c9897931d39",
+    "url": "/portfolio/static/js/runtime-main.d5e9ffa1.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/portfolio/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.767e19012dc021ba8fc8f223d12c5cbc.js"
+  "/portfolio/precache-manifest.86ce29d8b951fd9f782b766166d363e7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
